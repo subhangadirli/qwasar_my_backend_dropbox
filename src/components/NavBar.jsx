31fe6e72748fc9ee3cfc0b0df1382e4ada0f5654
hajibala_n <0.0000.void.00000.0@gmail.com>
2026-07-23 19:56:09 +0400

    Add NavBar placeholder component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <span className="nav-bar-brand">My Backend Dropbox</span>
+    </nav>
+  );
+}
+
+export default NavBar;

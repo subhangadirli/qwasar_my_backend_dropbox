4b30bd4e72293366b3fc0ef158a0185bc6f7ef78
hajibala_n <0.0000.void.00000.0@gmail.com>
2026-07-23 19:56:32 +0400

    Add FileList placeholder component

---
+++ src/components/FileList.jsx
@@ -0,0 +1,11 @@
+import "./FileList.css";
+
+function FileList() {
+  return (
+    <section className="file-list">
+      <p className="file-list-empty">No files yet.</p>
+    </section>
+  );
+}
+
+export default FileList;
